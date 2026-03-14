5bb07601359f48c1a5e5e98484f1afb045d9c484
andreevil2007-code <andreev.il.2007@gmail.com>
2026-03-14 10:37:09 +0300

    Добавлены настройки команды: vsconfig, launchSettings, editorconfig

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello from branch!");
+
+if (true)
+{
+    var message = "This has wrong indentation";
+    Console.WriteLine(message);
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"Line {i}");
+    }
+}
